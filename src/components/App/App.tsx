dc9abd3a74a361c952e8dd620fb8fda2819cdbac
Tristan Chin <tristan.chin@chintristan.io>
2022-04-22 02:38:34 -0400

    Initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Button } from "@mantine/core";
+
+export default () => {
+    return <Button>Hello world!</Button>;
+};
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App/App";
+
+const root = createRoot(document.getElementById("root")!);
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
