7a883a829b4a0d1dcf9a63ebb0cf87c7e4523a7e
Tristan Chin <tristan.chin@chintristan.io>
2022-05-03 22:45:30 -0400

    Add MantineProviders

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { MantineProvider, TypographyStylesProvider } from "@mantine/core";
 import App from "./components/App/App";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
     <StrictMode>
-        <App />
+        <TypographyStylesProvider>
+            <MantineProvider withNormalizeCSS withGlobalStyles>
+                <App />
+            </MantineProvider>
+        </TypographyStylesProvider>
     </StrictMode>
 );
