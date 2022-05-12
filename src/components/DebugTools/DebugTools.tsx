7c1dd13463f4c827003474afb96c5175a23b067b
Tristan Chin <tristan.chin@chintristan.io>
2022-05-11 21:31:24 -0400

    Add changeLanguage to DebugTools

--- src/components/DebugTools/DebugTools.tsx
+++ src/components/DebugTools/DebugTools.tsx
@@ -1,5 +1,6 @@
-import { useMantineColorScheme } from "@mantine/core";
 import React from "react";
+import { useMantineColorScheme } from "@mantine/core";
+import { changeLanguage } from "i18next";
 
 export default () => {
     if (process.env.NODE_ENV !== "development") {
@@ -10,6 +11,7 @@ export default () => {
 
     React.useEffect(() => {
         (window as any).toggleColorScheme = toggleColorScheme;
+        (window as any).changeLanguage = changeLanguage;
     }, []);
 
     return null;
