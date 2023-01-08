2a2f87a5064ce2fedc81ec13d6ffb2cadecbb0da
Tristan Chin <tristan.chin@chintristan.io>
2023-01-07 20:10:08 -0500

    Fix education image positionning on mobile

--- src/pages/Home/Education/Education.tsx
+++ src/pages/Home/Education/Education.tsx
@@ -1,4 +1,5 @@
-import { Card, Group } from "@mantine/core";
+import { Card, Center, Group, Stack, useMantineTheme } from "@mantine/core";
+import { useViewportSize } from "@mantine/hooks";
 import React from "react";
 import EducationImage from "./EducationImage";
 import EducationInfo from "./EducationInfo";
@@ -9,12 +10,32 @@ interface EducationProps {
 }
 
 export default ({ education }: EducationProps) => {
+    const theme = useMantineTheme();
+    const { width } = useViewportSize();
+
+    const isDesktopSize = width >= theme.breakpoints.sm;
+
     return (
         <Card shadow="md" radius="lg">
-            <Group>
-                <EducationImage name={education.name} image={education.image} />
-                <EducationInfo education={education} />
-            </Group>
+            {isDesktopSize ? (
+                <Group>
+                    <EducationImage
+                        name={education.name}
+                        image={education.image}
+                    />
+                    <EducationInfo education={education} />
+                </Group>
+            ) : (
+                <Stack>
+                    <Center>
+                        <EducationImage
+                            name={education.name}
+                            image={education.image}
+                        />
+                    </Center>
+                    <EducationInfo education={education} />
+                </Stack>
+            )}
         </Card>
     );
 };
--- src/pages/Home/Education/EducationImage.tsx
+++ src/pages/Home/Education/EducationImage.tsx
@@ -1,4 +1,4 @@
-import { createStyles, Image } from "@mantine/core";
+import { Image } from "@mantine/core";
 import React from "react";
 
 interface EducationImageProps {
@@ -6,32 +6,12 @@ interface EducationImageProps {
     name: string;
 }
 
-const useStyles = createStyles((theme) => ({
-    image: {
-        width: "100%",
-        display: "flex",
-        justifyContent: "center",
-
-        [`@media (min-width: ${theme.breakpoints.sm}px)`]: {
-            width: "unset",
-            display: "block",
-        },
-
-        "& img": {
-            marginBottom: 0,
-        },
-    },
-}));
-
 const W = 128;
 const H = W / 2;
 
 export default ({ image, name }: EducationImageProps) => {
-    const { classes } = useStyles();
-
     return (
         <Image
-            className={classes.image}
             radius={0}
             width={W}
             height={H}
